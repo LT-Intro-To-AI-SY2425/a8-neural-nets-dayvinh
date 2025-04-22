24b8e625a4a9426f848e59104aef503a20e31a15
Davin Nguyen <davin.nguyen2017@gmail.com>
2025-04-22 10:37:28 -0500

    xorn

--- neural_test.py
+++ neural_test.py
@@ -118,3 +118,8 @@ xor_training_data = [([1, 1], [0]), ([1, 0], [1]), ([0, 1], [1]), ([0, 0], [0])]
 xorn = NeuralNet(2, 1, 1)
 xorn.train(xor_training_data)
 print(xorn.test_with_expected(xor_training_data))
+
+
+xorn = NeuralNet(2, 2, 1)
+xorn.train(xor_training_data, learning_rate=.2)
+print(xorn.test_with_expected(xor_training_data))
